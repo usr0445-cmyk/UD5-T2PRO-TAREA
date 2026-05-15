f917c2af1f0dc30a62f7a7307b47e13a31d7b1ec
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-15 18:48:39 +0100

    Un metodo que lo que hara es ordenar la puntuación de manera descendente, aparte al principio un breve texto de la clase

--- src/net/salesianos/ListaInteligente/listaInteligente.java
+++ src/net/salesianos/ListaInteligente/listaInteligente.java
@@ -2,6 +2,8 @@ package net.salesianos.ListaInteligente;
 import net.salesianos.Videojuego.videojuego;
 import java.util.ArrayList;
 
+// Esta clase gestiona un lista de videojuegos
+
 public class listaInteligente {
 
     private ArrayList<videojuego> lista = new ArrayList<>();
@@ -24,6 +26,20 @@ public class listaInteligente {
 
     public ArrayList<videojuego> obtenerTodos(){
         return lista;
+    }
+
+    public void ordenarPorPuntuacionDesc() {
+        for (int i = 0; i < lista.size() - 1; i++) {
+            for (int j = 0; j < lista.size() - 1 - i; j++) {
+
+                if (lista.get(j).getPuntuacion() < lista.get(j + 1).getPuntuacion()) {
+
+                    videojuego caja = lista.get(j);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, caja);
+                }
+            }
+        }
     }
 
 }
