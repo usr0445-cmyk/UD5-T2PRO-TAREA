278c33ad194b7789d147997af06e0c0fe3fe9fd3
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-15 14:03:25 +0100

    He creado los atributos necesarios y la de publicación, aparte mas adelante hice el constructor padre.

--- src/net/salesianos/Videojuego/videojuego.java
+++ src/net/salesianos/Videojuego/videojuego.java
@@ -2,4 +2,15 @@ package net.salesianos.Videojuego;
 
 public class videojuego {
 
+    private String titulo;
+    private String genero;
+    private int año;
+    private int puntuacion;
+
+    public videojuego(String titulo, String genero, int año, int puntuacion) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.año = año;
+        this.puntuacion = puntuacion;
+    }
 }
