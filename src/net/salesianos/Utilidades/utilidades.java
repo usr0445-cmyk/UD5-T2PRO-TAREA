ad63cf908e488dca97b7076599d364ba6ea8a5a7
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-15 19:57:11 +0100

    Importamos la biblioteca Swing para poder usarlo como nos pide el requisito

--- src/net/salesianos/Utilidades/utilidades.java
+++ src/net/salesianos/Utilidades/utilidades.java
@@ -1,6 +1,10 @@
 package net.salesianos.Utilidades;
 
+import javax.swing.JOptionPane;
+
 public class utilidades {
     
     // Esta clase es para la validacion y la solicitud de datos de usuario
+
+
 }
