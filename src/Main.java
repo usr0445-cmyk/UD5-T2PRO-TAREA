79203310c30cfc479e2e1f901103dc9c1ef539d5
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-16 20:22:39 +0100

    Estructurando la app (clase Main)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import net.salesianos.ListaInteligente.listaInteligente;
+import net.salesianos.Videojuego.videojuego;
+import net.salesianos.Utilidades.utilidades;
+import javax.swing.JOptionPane;
+
+
+public class Main {
+    
+}
