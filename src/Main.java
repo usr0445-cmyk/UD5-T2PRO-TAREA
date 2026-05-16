e371f130494a9a05ca2bba295eaec997f1c2b30f
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-16 20:33:58 +0100

    He usado la nueva libreria para mostrar un menu para el usuario

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,41 @@ import net.salesianos.Videojuego.videojuego;
 import net.salesianos.Utilidades.utilidades;
 import javax.swing.JOptionPane;
 
+//Esta es la calse principal del programa
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        ListaInteligente lista = new ListaInteligente();
+        boolean salir = false;
+
+        while (!salir) {
+
+            String opcion = JOptionPane.showInputDialog(
+                "MENU PRINCIPAL" +
+                "1. Agregar videojuego" +
+                "2. Editar videojuego" +
+                "3. Eliminar videojuego" +
+                "4. Mostrar lista ordenada" +
+                "5. Salir"
+            );
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
