6c21f8099104fa9aa8a15d095a73164703df541f
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-17 19:46:34 +0100

    Estructurado un poco mejor

--- src/Main.java
+++ src/Main.java
@@ -16,65 +16,65 @@ public class Main {
 
         while (!salir) {
 
-            //Mostrara una ventana con un menu de opciones para el usuario
-            String opcion = JOptionPane.showInputDialog(
-                "MENU PRINCIPAL" +
-                "1. Agregar videojuego" +
-                "2. Editar videojuego" +
-                "3. Eliminar videojuego" +
-                "4. Mostrar lista ordenada" +
-                "5. Salir"
-            );
+        //Mostrara una ventana con un menu de opciones para el usuario
+        String opcion = JOptionPane.showInputDialog(
+            "MENU PRINCIPAL" +
+            "1. Agregar videojuego" +
+            "2. Editar videojuego" +
+            "3. Eliminar videojuego" +
+            "4. Mostrar lista ordenada" +
+            "5. Salir"
+        );
 
-            //Si el usuario cierra la ventana, se saldra del programa, por eso el null
-            if (opcion == null) {
-                salir = true;
-                //Usamos equals porque es un string
-            } else if (opcion.equals("1")) {
+        //Si el usuario cierra la ventana, se saldra del programa, por eso el null
+        if (opcion == null) {
+            salir = true;
+            //Usamos equals porque es un string
+        } else if (opcion.equals("1")) {
 
-                //El metodo utilidades muestra el JOptionPane y validan para que el usuario no rompa esto
-                String titulo = utilidades.pedirTexto("Titulo: ");
-                String genero = utilidades.pedirTexto("Genero: ");
-                int año = utilidades.pedirEntero("Año: ");
-                int puntuacion = utilidades.pedirEntero("Puntuacion: ");
+        //El metodo utilidades muestra el JOptionPane y validan para que el usuario no rompa esto
+        String titulo = utilidades.pedirTexto("Titulo: ");
+        String genero = utilidades.pedirTexto("Genero: ");
+        int año = utilidades.pedirEntero("Año: ");
+        int puntuacion = utilidades.pedirEntero("Puntuacion: ");
 
-                //Creamos el objeto videojuego con los datos ingresados y despues se añadira a la lista
-                    videojuego v = new videojuego(titulo, genero, año, puntuacion);
-                    lista.agregarVideojuego(v);
+        //Creamos el objeto videojuego con los datos ingresados y despues se añadira a la lista
+        videojuego v = new videojuego(titulo, genero, año, puntuacion);
+        lista.agregarVideojuego(v);
                
-                } else if (opcion.equals("2")) {
+        } else if (opcion.equals("2")) {
 
-                    //Indice para que pueda edirar el juego
-                    int index = utilidades.pedirEntero("Ingrese el indice del videojuego a editar: ");
+        //Indice para que pueda edirar el juego
+        int index = utilidades.pedirEntero("Ingrese el indice del videojuego a editar: ");
 
-                    String titulo = utilidades.pedirTexto("Nuevo titulo: ");
-                    String genero = utilidades.pedirTexto("Nuevo genero: ");
-                    int año = utilidades.pedirEntero("Nuevo año: ");
-                    int puntuacion = utilidades.pedirEntero("Nueva puntuacion: ");
+        String titulo = utilidades.pedirTexto("Nuevo titulo: ");
+        String genero = utilidades.pedirTexto("Nuevo genero: ");
+        int año = utilidades.pedirEntero("Nuevo año: ");
+        int puntuacion = utilidades.pedirEntero("Nueva puntuacion: ");
 
 
-                    videojuego v = new videojuego(titulo, genero, año, puntuacion);
+        videojuego v = new videojuego(titulo, genero, año, puntuacion);
 
-                    //Se reemplaza el videojuego en la lista
-                    lista.editarVideojuego(index, v);
+        //Se reemplaza el videojuego en la lista
+        lista.editarVideojuego(index, v);
 
-                } else if (opcion.equals("3")) {
+        } else if (opcion.equals("3")) {
 
-                    int index = utilidades.pedirEntero("Indice que se va a elminar:");
-                    lista.eliminarVideojuego(index);
+        int index = utilidades.pedirEntero("Indice que se va a elminar:");
+        lista.eliminarVideojuego(index);
 
-                } else if (opcion.equals("4")) {
+        } else if (opcion.equals("4")) {
 
-                    //Ordena la lista por puntuacion de mayor a menor y luego muestra la lista ordenada con JOptionPane
-                    lista.ordenarPorPuntuacionDesc();
-                    JOptionPane.showMessageDialog(null, lista.obtenerTodos());
+        //Ordena la lista por puntuacion de mayor a menor y luego muestra la lista ordenada con JOptionPane
+        lista.ordenarPorPuntuacionDesc();
+        JOptionPane.showMessageDialog(null, lista.obtenerTodos());
      
 
-                    //Nos salimos
-                } else if (opcion.equals("5")) {
-                    salir = true;
-                }
-            }
+            //Nos salimos
+        } else if (opcion.equals("5")) {
+            salir = true;
+        }
+        }
 
         }
 
