5f741c01983976f4ce1b811cfb305d79d38215e9
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-17 19:22:24 +0100

    He añadido un par de comentarios en la clase principal

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,15 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
+        //Aqui usamos la lista inteligente donde se guardaran los videojuegos
         ListaInteligente lista = new ListaInteligente();
+
+        //Una variable que controla cuando salir del programa
         boolean salir = false;
 
         while (!salir) {
 
+            //Mostrara una ventana con un menu de opciones para el usuario
             String opcion = JOptionPane.showInputDialog(
                 "MENU PRINCIPAL" +
                 "1. Agregar videojuego" +
@@ -22,20 +26,25 @@ public class Main {
                 "5. Salir"
             );
 
+            //Si el usuario cierra la ventana, se saldra del programa, por eso el null
             if (opcion == null) {
                 salir = true;
+                //Usamos equals porque es un string
             } else if (opcion.equals("1")) {
 
+                //El metodo utilidades muestra el JOptionPane y validan para que el usuario no rompa esto
                 String titulo = utilidades.pedirTitulo("Titulo: ");
                 String genero = utilidades.pedirGenero("Genero: ");
                 int año = utilidades.pedirAño("Año: ");
                 int puntuacion = utilidades.pedirPuntuacion("Puntuacion: ");
 
+                //Creamos el objeto videojuego con los datos ingresados y despues se añadira a la lista
                     videojuego v = new videojuego(titulo, genero, año, puntuacion);
                     lista.agregarVideojuego(v);
                
                 } else if (opcion.equals("2")) {
 
+                    //Indice para que pueda edirar el juego
                     int index = utilidades.pedirEntero("Ingrese el indice del videojuego a editar: ");
 
                     String titulo = utilidades.pedirTitulo("Nuevo titulo: ");
@@ -43,7 +52,10 @@ public class Main {
                     int año = utilidades.pedirAño("Nuevo año: ");
                     int puntuacion = utilidades.pedirPuntuacion("Nueva puntuacion: ");
 
+
                     videojuego v = new videojuego(titulo, genero, año, puntuacion);
+
+                    //Se reemplaza el videojuego en la lista
                     lista.editarVideojuego(index, v);
 
                 } else if (opcion.equals("3")) {
@@ -52,10 +64,13 @@ public class Main {
                     lista.eliminarVideojuego(index);
 
                 } else if (opcion.equals("4")) {
+
+                    //Ordena la lista por puntuacion de mayor a menor y luego muestra la lista ordenada con JOptionPane
                     lista.ordenarPorPuntuacionDesc();
                     JOptionPane.showMessageDialog(null, lista.obtenerTodos());
      
 
+                    //Nos salimos
                 } else if (opcion.equals("5")) {
                     salir = true;
                 }
